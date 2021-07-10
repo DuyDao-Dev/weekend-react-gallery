a18a58e3f9b4cf7cb579632dec55f7b7db9f1da9
Duy Dao <jimmydao@gmail.com>
2021-07-09 22:24:57 -0500

    Added photos and descriptions to gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,35 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/benedict.jpg",
+    description:
+      "Eggs benedict 2 ways. Front one is with collard greens and second one is a Reuben benedict.",
+    likes: 0,
+  },
+  {
+    id: 1,
+    path: "images/chefbreakfast.jpg",
+    description: "This is your typical breakfast in Italy. Espresso, lemon cream filled croissant, orange juice, and a smoke.",
+    likes: 0,
+  },
+  {
+    id: 1,
+    path: "images/dessert.jpg",
+    description: "Deconstructed smores I had at Red Wagon Pizza in Minneapolis.",
+    likes: 0,
+  },
+  {
+    id: 1,
+    path: "images/luckyoven.jpg",
+    description: "The day my daughter Zoey visited me at work at Lucky Oven Bakery.",
+    likes: 0,
+  },
+  {
+    id: 1,
+    path: "images/tartare.jpg",
+    description: "Made the staff some steak tartare with delicious scraps after making steaks.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
